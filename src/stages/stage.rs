37b2e5fd9752ffb3293491d0e6f12f940ebbd77c
abissens <hichem.abissens@gmail.com>
2021-11-07 16:55:38 +0100

    Remove unused pipeline struct

--- src/stages/stage.rs
+++ src/stages/stage.rs
@@ -2,10 +2,6 @@ use crate::pages::PageBundle;
 use std::any::Any;
 use std::sync::Arc;
 
-pub struct Pipeline {
-    pub stages: Vec<Box<dyn Stage>>,
-}
-
 pub trait Stage: Send + Sync {
     fn process(&self, bundle: &Arc<dyn PageBundle>) -> anyhow::Result<Arc<dyn PageBundle>>;
     fn as_any(&self) -> &dyn Any {
